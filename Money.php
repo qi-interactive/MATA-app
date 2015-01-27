99e5cbe1773088fe29d9fb14b4c7b0de996744d4
Marcin Wiatr <marcin@qi-interactive.com>
2015-01-27 10:40:39 +0000

    updated tests
    Signed-off-by: Marcin Wiatr <marcin@qi-interactive.com>

---
+++ Money.php
@@ -0,0 +1,10 @@
+<?php
+
+class Money {
+
+	public static function add($a, $b) {
+	return $a + $b;
+}
+
+
+}
--- MoneyTest.php
+++ MoneyTest.php
@@ -1,12 +1,14 @@
 <?php
+
+require "Money.php";
+
 class MoneyTest extends PHPUnit_Framework_TestCase
 {
 
     public function testCanBeNegated()
     {
-	$this->assertTrue(true);
+	$this->assertEquals(4, Money::add(2, 2));
     }
 
-    // ...
 }
 
