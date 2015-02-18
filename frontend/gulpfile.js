f135c74f1301eee63627e32ff3ce9624a681286a
Marcin Wiatr <marcin@qi-interactive.com>
2015-02-18 22:01:10 +0000

    Removed comments from gulpfile
    Signed-off-by: Marcin Wiatr <marcin@qi-interactive.com>

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -7,8 +7,6 @@ plumber = require('gulp-plumber'),
 gutil = require('gulp-util'),
 lessPluginCleanCSS = require('less-plugin-clean-css');
 
-var sourcemaps = require('gulp-sourcemaps');
-
 var cleanCSS = new lessPluginCleanCSS({ 
 	advanced: true,
 	keepSpecialComments : 0,
@@ -20,68 +18,27 @@ var options = {
 } 
 
 gulp.task('less', function() {
-
-	// gulp.src(['**/assets/less/*.less'])
-	// .pipe(less())
-	// .pipe(rename(function (path) {
-	// 	path.dirname = path.dirname.replace("/less", "/css")
-	// }))
-	// .pipe(gulp.dest("."))
-
-	console.log("LESSING")
-	gulp.src(['assets/less/**/*.less', '!assets/less/inuit.css/**/*'])
+	gulp.src(['assets/less/**/*.less', '!assets/less/inuit.css/**/*', '!assets/less/vars.less'])
 	.pipe(plumber(handleError))
 	.pipe(less({
-		paths: [ path.join(__dirname, 'less', 'less/inuit.css') ],
+		paths: [ path.join(__dirname, 'less', 'less/inuit.css', 'less/vars.less') ],
 		plugins: [cleanCSS]
 	}))
-	.pipe(gulp.dest("./web/css"))
+	.pipe(gulp.dest("./web/css")).
+	pipe(livereload())
 })
 
 gulp.task('watch', function() {
 
-	// watch(['commands/**/*.php', 'config/**/*.php', 'controllers/**/*.php', 'components/*.php', 'helpers/**/*.php', 'mail/**/*.php',
-	// 	'models/**/*.php', 'modules/**/*.php', 'modules/**/assets/**/*.{css,js}', 'views/**/*.php' ,'web/**/*.php', 'widgets/**/*.php', 
-	// 	'!web/css/**/*.css', 'web/images/**/*.{png, jpg, gif}', 'web/js/*.js',
-	// 	'widgets/**/js/**/*.js', 'widgets/**/assets/css/*.css', 'widgets/**/web/*.css', 'assets/*.php'], {
-
-	// 		name: "Watcher",
-	// 		verbose: true
-	// 	})
-
-	// .pipe(livereload())
-
+	livereload.listen();
 
 	watch(['**/assets/less/*.less', 'assets/less/**/*.less', '!assets/less/inuit.css/*.less'], {
-
-		name: "LESS Watcher",
+		name: "Watcher",
 		verbose: true
 	}, function() {
-			// console.log(123123)
-			gulp.start('less')
-		})
-	// .pipe(watch(['**/assets/less/*.less', 'assets/less/**/*.less', '!assets/less/inuit.css/*.less']))
-	// .pipe(less({
-	// 		paths: [ path.join(__dirname, 'less', 'less/inuit.css') ]
-	// 	}))
-	// .pipe(gulp.dest("./web/css"))
-	// .pipe(livereload())
-
-
-
-	// gulp.src(['**/assets/less/*.less', 'assets/less/**/*.less', '!assets/less/imports/*.less'])
-	// .pipe(plumber(handleError))
-	// .pipe(watch(['**/assets/less/*.less', 'assets/less/**/*.less', '!assets/less/imports/*.less']))
-	// .pipe(less())
-	// .pipe(gulp.dest("./web/css"))
-	// .pipe(livereload())
-
-
+		gulp.start('less');
+	})
 });
-
-function getUserHome() {
-	return process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
-}
 
 function handleError(err) {
 	var displayErr = gutil.colors.red(err);
