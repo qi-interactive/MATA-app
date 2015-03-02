97a10abff4b410861e95112fbadbb3a66cd580db
Marcin Wiatr <marcin@qi-interactive.com>
2015-03-02 19:22:24 +0000

    Based matacms MessageEvent on mata MessageEvent
    Signed-off-by: Marcin Wiatr <marcin@qi-interactive.com>

--- mata-cms/base/MessageEvent.php
+++ mata-cms/base/MessageEvent.php
@@ -2,27 +2,21 @@
 
 namespace matacms\base;
 
-class MessageEvent extends \yii\base\Event {
+class MessageEvent extends \mata\base\MessageEvent {
 
-	const LEVEL_SUCCESS = "success";
-	const LEVEL_INFO = "info";
-	const LEVEL_WARNING = "danger";
-	const LEVEL_ERROR = "danger";
+    const LEVEL_SUCCESS = "success";
+    const LEVEL_INFO = "info";
+    const LEVEL_WARNING = "danger";
+    const LEVEL_ERROR = "danger";
 
-    private $message;
     private $level;
 
     public function __construct($message, $level = self::LEVEL_SUCCESS) {
-    	$this->message = $message;
-    	$this->level = $level;
-    	parent::__construct([]);
-    }
-
-    public function getMessage() {
-    	return $this->message;
+        $this->level = $level;
+        parent::__construct($message);
     }
 
     public function getLevel() {
-    	return $this->level;
+        return $this->level;
     }
 }
