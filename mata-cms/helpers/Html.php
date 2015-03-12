672e4bd2868552335895acd155ffee46f2232c6e
Marcin Wiatr <marcin@qi-interactive.com>
2015-03-12 17:31:28 +0000

    added activeMediafield
    Signed-off-by: Marcin Wiatr <marcin@qi-interactive.com>

--- mata-cms/helpers/Html.php
+++ mata-cms/helpers/Html.php
@@ -8,6 +8,8 @@ use mata\category\models\CategoryItem;
 use mata\tag\models\Tag;
 use mata\tag\models\TagItem;
 use yii\selectize\Selectize;
+use \mata\widgets\fineuploader\Fineuploader;
+use mata\media\models\Media;
 
 class Html extends \yii\helpers\Html {
 
@@ -46,16 +48,27 @@ class Html extends \yii\helpers\Html {
 		$options["name"] = TagItem::REQ_PARAM_TAG_ID;
 
 		$options = ArrayHelper::merge([
-		      'items' => $items,
-		      'options' => ['multiple'=>true],
-		      'clientOptions' => [
-		      'plugins' => ["remove_button", "drag_drop", "restore_on_backspace"],
-		      	'create' => true,
-		      	'persist' => false,
-		      ]
-		      ], $options);
+			'items' => $items,
+			'options' => ['multiple'=>true],
+			'clientOptions' => [
+			'plugins' => ["remove_button", "drag_drop", "restore_on_backspace"],
+			'create' => true,
+			'persist' => false,
+			]
+			], $options);
 
 		return Selectize::widget($options);
+	}
+
+	public static function activeMediaField($model, $attribute, $options = []) {
+		return Fineuploader::widget([
+			'name' => $model->getDocumentId(),
+			'model' => $model,
+			'attribute' => $attribute,
+			'options' => $options,
+			'uploadSuccessEndpoint' => "/mata-cms/media/s3/upload-successful?documentId=" . urlencode($model->getDocumentId() . "::" . $attribute)
+			]);
+
 	}
 
 }
