2680c4d34b9925321a595fcc6003371804376c0a
Michal Fiedorowicz <michal@qi-interactive.com>
2015-07-23 13:57:23 +0100

    Update

--- common/config/main.php
+++ common/config/main.php
@@ -24,6 +24,21 @@ return [
     'modules' => [
         'user' => [
             'class' => 'matacms\user\Module',
+            'modelMap' => [
+                'User'             => 'matacms\user\models\User',
+                'Account'          => 'matacms\user\models\Account',
+                'Profile'          => 'matacms\user\models\Profile',
+                'Token'            => 'matacms\user\models\Token',
+                'RegistrationForm' => 'matacms\user\models\RegistrationForm',
+                'ResendForm'       => 'matacms\user\models\ResendForm',
+                'LoginForm'        => 'matacms\user\models\LoginForm',
+                'SettingsForm'     => 'matacms\user\models\SettingsForm',
+                'RecoveryForm'     => 'matacms\user\models\RecoveryForm',
+                'UserSearch'       => 'matacms\user\models\UserSearch',
+            ],
+            'mailer' => [
+                'sender' => 'notifications@matacms.com'
+            ]
         ],
         'rbac' => [
             'class' => 'matacms\rbac\Module',
